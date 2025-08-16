4c90e1739975bbda26476a6821531395b1774652
maxpeshkun <73502043+maxpeshkun@users.noreply.github.com>
2025-08-16 10:35:15 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, output: 'export' }
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  // НИЧЕГО про output: 'export' не указываем
+};
+module.exports = nextConfig;
